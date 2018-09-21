f025634aa93d8faf7432983b15794c07697c610b
smudge37 <stephenm.jackson@btinternet.com>
2018-09-21 11:06:07 +0100

    Fixed stringCleaner

--- src/stringutil/StringCleaner.java
+++ src/stringutil/StringCleaner.java
@@ -4,6 +4,6 @@ package stringutil;
 public class StringCleaner {
     public String cleanNonLetters(String str) {
         String regex = "[^a-z]";
-        str.replaceAll(regex, "");
+        return str.replaceAll(regex, "");
     }
 }
